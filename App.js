22dca15463b0382c916c906a2ee461035e081a12
tangjunqiu <tangjqwysj@sina.com>
2017-12-13 19:31:42 +0800

    复制粘贴

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.copyUrl2 = this.copyUrl2.bind(this);
+    this.reftext = this.reftext.bind(this)
+  }
+  copyUrl2(){
+    const texta = this.texta
+    texta.select()
+    document.execCommand("Copy");
+    console.log('fuzhi')
+  }
+  reftext(node){
+    this.texta = node
+  }
+  render() {
+    return (
+      <div>
+        <input ref={this.reftext} defaultValue='fffffffffffffff' disabled style={{background:'#fff',border:0}}></input>
+        <input type="button" onClick={this.copyUrl2} value="点击复制代码" />
+      </div>
+    );
+  }
+}
+
+export default App
